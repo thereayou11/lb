35ed4b6f066e1884ce0b57190bc11637cfc4e600
thereayou11 <73603698+thereayou11@users.noreply.github.com>
2020-12-18 16:29:55 +0200

    Add files via upload

---
+++ lb4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+float func(float);
+
+int main() {
+	printf("%.2f", func(111.3333));
+
+	return 0;
+}
+
+float func(float a) {
+	int integer = (int)a;
+	float fractional = a - integer;
+
+	fractional *= 10000;
+	float result = fractional / integer;
+
+	return result;
+}
