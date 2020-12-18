0bacfa3363cb2a5f961ee4b2372fa2a7172fe405
thereayou11 <73603698+thereayou11@users.noreply.github.com>
2020-12-18 17:10:29 +0200

    Add files via upload

---
+++ lb6.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void show(int arr[5][5]);
+
+int main() {
+	int tmp;
+	int arr[5][5];
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			arr[i][j] = j + 1;
+		}
+	}
+	show(arr);
+	cout << endl;
+	for (int i = 0; i < 5; i++) {
+		tmp = arr[i][0];
+		for (int j = 0; j < 4; j++) {
+			arr[i][j] = arr[i][j + 1];
+		}
+		arr[i][4] = tmp;
+
+	}
+	show(arr);
+
+	return 0;
+}
+
+void show(int arr[5][5]) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			cout << arr[i][j] << " ";
+		}
+		std::cout << endl;
+	}
+}
