aa4704a705ad30874546aa7ea343e842a05325f6
thereayou11 <73603698+thereayou11@users.noreply.github.com>
2020-12-18 16:43:52 +0200

    Add files via upload

---
+++ lb5.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace tripls;
+
+bool funcPrimeCheckFor(int);
+bool funcPrimeCheckDoWhile(int);
+bool funcPrimeCheckWhile(int);
+
+
+
+int main() {
+	cout << funcPrimeCheckWhile(9) << endl;
+	return 0;
+}
+
+bool funcPrimeCheckFor(int digit) {
+	bool f = true;
+	for (int i = 2; i < digit; i++) {
+		if (digit % i == 0) {
+			f = false;
+			break;
+		}
+	}
+
+	return f;
+}
+
+bool funcPrimeCheckWhile(int digit) {
+	bool f = true;
+	int i = 2;
+	do {
+		if (digit % i == 0) {
+			f = false;
+			break;
+		}
+		i++;
+	} while (i < digit);
+
+	return f;
+}
+
+
+bool funcPrimeCheckDoWhile(int digit) {
+	bool f = true;
+	int i = 2;
+	while (i < digit) {
+		if (digit % i == 0) {
+			f = false;
+			break;
+		}
+		i++;
+	}
+	return f;
+}
