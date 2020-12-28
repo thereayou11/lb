e52c02ecfcdd392d94807190386292647868cc80
thereayou11 <73603698+thereayou11@users.noreply.github.com>
2020-12-28 14:36:08 +0200

    Add files via upload

---
+++ lb13.cpp
@@ -0,0 +1,17 @@
+﻿#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string s;
+	getline(cin, s);
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] >= '0' && s[i] <= '9')
+			cout << s[i];
+		else if (!(s[i] >= '0' && s[i] <= '9') && s[i + 1] >= '0' && s[i + 1] <= '9')
+			cout << " ";
+
+	}
+	return 0;
+}
